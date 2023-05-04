3e70abdb594a9c796fd9a50dac54fcd21ee0202a
PlatonKynin <kininplaton@gmail.com>
2023-05-04 23:02:40 +0300

    Задача c курса Object

--- 3.2ПростыеОбъектыСДанными/main.js
+++ 3.2ПростыеОбъектыСДанными/main.js
@@ -1,4 +1,5 @@
 // Objects
+/*
 const developer = {
    name: 'Vasiliy',
    surname: 'Vasiliy',
@@ -11,4 +12,24 @@ developer.isMaried = true;
 
 delete developer.isMaried;
 
-console.log(developer.isMaried);
+console.log(developer.isMaried);*/
+
+
+
+//Задача c курса
+//Создание и изменение объекта
+// Создайте объект dog. Назначьте ему свойства со следующими ключами: name, age, color, weight.
+
+// После создания объекта на отдельной строчке добавьте объекту дополнительное свойство с ключом breed.
+
+
+const dog = {
+   name: 'Bysa',
+   age: 5,
+   color: 'white',
+   weight: '7kg',
+};
+
+dog.breed = 'English bulldog';
+
+console.log(dog);

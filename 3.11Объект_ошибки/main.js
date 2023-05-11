a02ef8a2191bdbe971752f9c80286b127550f86a
PlatonKynin <kininplaton@gmail.com>
2023-05-11 21:18:11 +0300

    Объект ошибки

---
+++ 3.11Объект_ошибки/main.js
@@ -0,0 +1,17 @@
+//errors
+
+function sum(a, b) {
+   if(typeof a!== 'number' || typeof b !== 'number') {
+      throw new Error('В функцию не переданы числа для сложения');
+   }
+   return a + b;
+}
+
+try {
+   sum();
+   console.log('not seen')
+} catch (err) {
+   console.error(err.message);
+}
+
+console.log('end');

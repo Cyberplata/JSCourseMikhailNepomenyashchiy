ee81585e438c3cbce8d052912f5b7a8822ef5d25
PlatonKynin <kininplaton@gmail.com>
2023-05-11 23:56:02 +0300

    Проект вместе с подсказками и попытался try..catch

--- 3.13Проект_Загадки/main.js
+++ 3.13Проект_Загадки/main.js
@@ -27,24 +27,24 @@ var riddle = {
    hints: ['это несъедобное', 'это не фрукт'],
    tries: 3,
    checkAnswer(guessedAnswer) {
-       // TODO: написать логику проверки правильного ответа
-       // alert для пользователя, console.log()
-       if(this.correctAnswer === guessedAnswer) {
+      // TODO: написать логику проверки правильного ответа
+      // alert для пользователя, console.log()
+      if (this.correctAnswer === guessedAnswer) {
          alert(`Правильный ответ`);
          console.log(`Правильный ответ`);
          this.tries = 0;
-       } else if (this.correctAnswer !== guessedAnswer && this.tries !== 0) {
-         alert(`Неправильный ответ, ${this.tries - 1},`);
-         console.log(`Неправильный ответ, ${this.tries - 1}`);
+      } else if (this.correctAnswer !== guessedAnswer && this.tries !== 0) {
+         alert(`Неправильный ответ, осталось попыток: ${this.tries - 1}. \nПодсказка: ${this.hints.join(' и ')}`);
+         console.log(`Неправильный ответ, осталось попыток: ${this.tries - 1}. \nПодсказка: ${this.hints.join(' и ')}`);
          this.tries--;
-       } else {
-         console.log(`Игра окончена`);
-         return alert('Игра окончена');
-       }
+      } else {
+         console.log(`Игра окончена, правильный ответ: ${this.correctAnswer}`);
+         return alert(`Игра окончена, правильный ответ: ${this.correctAnswer}`);
+      }
    },
 }
 
-window.onload = function() {
+window.onload = function () {
    document.getElementById('riddle').innerText = riddle.question;
 }
 
@@ -55,7 +55,18 @@ function check(guessedAnswer) {
 
 
    if (guessedAnswer) {
-       // TODO: вызвать функцию checkAnswer у объекта загадки, передав туда ответ
-       riddle.checkAnswer(guessedAnswer);
+      // TODO: вызвать функцию checkAnswer у объекта загадки, передав туда ответ
+      riddle.checkAnswer(guessedAnswer);
+         // if(typeof guessedAnswer !== 'string') {
+         //    throw new Error('Введите пожалуйста слово!');
+         // }
+         // return guessedAnswer;
    }
 }
+// try {
+//    check();
+//    console.log('not seen');
+// } catch (error) {
+//    console.log(error.message);
+// }
+// console.log('The end!');

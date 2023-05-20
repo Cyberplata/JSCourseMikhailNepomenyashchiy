1ab1f8f4c428e12022618d135ba275786e7d62a2
PlatonKynin <kininplaton@gmail.com>
2023-05-20 21:46:16 +0300

    Задача с ютуба от Михаила

--- 4.Про_функции_подробно/4.7Рекурсия/main.js
+++ 4.Про_функции_подробно/4.7Рекурсия/main.js
@@ -46,17 +46,58 @@ console.log(repeater('a2'));
 */
 
 
-function getLength(arr) {
-   let count = 0;
+// function getLength(arr) {
+//    let count = 0;
 
-   if(arr.pop() === undefined) { // Если мы вызовем pop() на пустом массив, он вернёт значение undefined
-      return count;
-   } else {
+//    if(arr.pop() === undefined) { // Если мы вызовем pop() на пустом массив, он вернёт значение undefined
+//       return count;
+//    } else {
+//       count++;
+//       //console.log(count);
+//       return count++ + getLength(arr);
+//    }
+// }
+
+
+// console.log(getLength([1, 2, 3, 5, 7]));
+
+
+//                                          Задача с ютуба от Михаила, решить рекурсией
+
+
+//обычное решение
+/*
+function sumOfDigits(numbers) {
+   let count = 0;
+   let last;
+
+   while(numbers !== 0) {
+      last = numbers % 10; // находим последнюю цифру
+      numbers = (numbers - last) / 10; // отбрасываем последние цифры
       count++;
-      //console.log(count);
-      return count++ + getLength(arr);
    }
+   return count;
 }
 
+console.log(sumOfDigits(100)) // 3
+console.log(sumOfDigits(1000)) // 4
+*/
 
-console.log(getLength([1, 2, 3, 5, 7]));
+
+// Рекурсия
+
+// let count = 0;
+// function sumOfDigits(numbers) {
+
+//    if (numbers < 1) {
+//       return;
+//    } else {
+//       count++;
+//       return sumOfDigits(numbers / 10), count;
+//    }
+// }
+
+
+
+console.log(sumOfDigits(100)) // 3
+console.log(sumOfDigits(1000)) // 4

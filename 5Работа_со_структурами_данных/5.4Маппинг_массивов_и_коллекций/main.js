bacc381c2ff428a8e6114f48e8358b69440e9338
PlatonKynin <kininplaton@gmail.com>
2023-05-28 23:09:07 +0300

    Практика с map

---
+++ 5Работа_со_структурами_данных/5.4Маппинг_массивов_и_коллекций/main.js
@@ -0,0 +1,74 @@
+// Mapping
+// map, forEach
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// const arr = [];
+// function doubleNum(num) {
+//    arr.push(num * 2);
+// }
+
+function print(el) {
+   console.log(el);
+}
+// const result = numbers.forEach(print);
+// console.log(result);
+// console.log(numbers);
+
+function doubleNum(num) {
+   return num * 2;
+}
+
+/*const result = numbers.map(num => num * 2); // [2, 4, 6 ...]
+console.log(result);
+console.log(numbers);*/
+
+
+
+//Дан массив чисел numbers. Создайте на его основе новый массив doubledNumbers, в котором каждый элемент из оригинального массива умножен на два.
+const numbers = [1, 2, 3];
+
+function mainCode(numbers) {
+   // напишите код до оператора return
+   const doubledNumbers = numbers.map(num => num * 2);
+   return doubledNumbers;
+}
+const result = mainCode(numbers);
+
+// console.log(result);
+// console.log(numbers);
+
+
+/*Дана коллекция developers. Создайте на ее основе новую коллекцию middleDevelopers, 
+увеличив значения ключа salary на 500, а также добавив к ключу skills значение 'TypeScript'. */
+
+const developers = [
+   {
+       id: 1,
+       fullName: 'Anton Petrov',
+       skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'React'],
+       salary: 1000,
+   },
+   {
+       id: 2,
+       fullName: 'Ivan Ivanov',
+       skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'Vue'],
+       salary: 950,
+   },
+   {
+       id: 3,
+       fullName: 'Albert Sidorov',
+       skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'jQuery'],
+       salary: 850,
+   },
+];
+
+const middleDevelopers = developers.map(function(newValues) {
+   return {
+      ...newValues,
+      salary: newValues.salary + 500,
+      skills: [...newValues.skills, 'TypeScript'],
+   };
+})
+
+console.log(middleDevelopers);

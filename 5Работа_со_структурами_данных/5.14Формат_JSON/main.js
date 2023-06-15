f16718b5e0bcafec68cc7891096e73b46aba4aaa
PlatonKynin <kininplaton@gmail.com>
2023-06-15 20:53:34 +0300

    Практика на JSON, коллекцию преобразовали в JSON

--- 5Работа_со_структурами_данных/5.14Формат_JSON/main.js
+++ 5Работа_со_структурами_данных/5.14Формат_JSON/main.js
@@ -33,3 +33,43 @@ const obj = {
 }
 
 console.log(JSON.stringify(obj)); // {"a":1}
+
+
+//Преобразование данных в формат JSON
+// Дана коллекция players. Создайте новую переменную jsonPlayers и сохраните в ней оригинальную коллекцию в формате JSON.
+
+const players = [
+   {
+       id: 1,
+       name: 'Andres',
+       surname: 'Iniesta',
+       club: 'Vissel Cobe',
+   },
+   {
+       id: 2,
+       name: 'Eden',
+       surname: 'Hazard',
+       club: 'Real Madrid',
+   },
+   {
+       id: 3,
+       name: 'Mo',
+       surname: 'Salah',
+       club: 'Liverpool',
+   },
+   {
+       id: 4,
+       name: 'Lionel',
+       surname: 'Messi',
+       club: 'Barcelona',
+   },
+];
+
+const jsonPlayers = JSON.stringify(players);
+console.log(jsonPlayers);
+/*
+[{"id":1,"name":"Andres","surname":"Iniesta","club":"Vissel Cobe"},
+{"id":2,"name":"Eden","surname":"Hazard","club":"Real Madrid"},
+{"id":3,"name":"Mo","surname":"Salah","club":"Liverpool"},
+{"id":4,"name":"Lionel","surname":"Messi","club":"Barcelona"}]
+*/

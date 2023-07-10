ee4fa383481d3867d609fe3737108da0b3b1af21
PlatonKynin <kininplaton@gmail.com>
2023-07-10 23:04:29 +0300

    6.13 Понятие всплытия

---
+++ 6.DOM_API/6.13Понятие_всплытия/app.js
@@ -0,0 +1,10 @@
+document.querySelector('button').addEventListener('click', handleClick);
+document.querySelector('section').addEventListener('click', handleClick);
+document.querySelector('div').addEventListener('click', handleClick);
+
+function handleClick(event) {
+   // event.stopPropagation();
+   console.log(event);
+   console.log(event.target);
+   console.log(event.currentTarget);
+}

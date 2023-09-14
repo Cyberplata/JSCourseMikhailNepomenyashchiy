516cb26b6fd6cfb39536629ba032c908fddcee42
PlatonKynin <kininplaton@gmail.com>
2023-09-14 21:35:43 +0300

    freecodecamp Profile Lookup

--- 5Работа_со_структурами_данных/5.4Маппинг_массивов_и_коллекций/main.js
+++ 5Работа_со_структурами_данных/5.4Маппинг_массивов_и_коллекций/main.js
@@ -90,5 +90,70 @@ function addNewCollection(obj) {
 const middleDevelopers = developers.map(addNewCollection);
 
 // console.log(developers);
-console.log(middleDevelopers);
+// console.log(middleDevelopers);
 // console.log(developers);
+
+
+// Setup
+const contacts = [
+   {
+     firstName: "Akira",
+     lastName: "Laine",
+     number: "0543236543",
+     likes: ["Pizza", "Coding", "Brownie Points"],
+   },
+   {
+     firstName: "Harry",
+     lastName: "Potter",
+     number: "0994372684",
+     likes: ["Hogwarts", "Magic", "Hagrid"],
+   },
+   {
+     firstName: "Sherlock",
+     lastName: "Holmes",
+     number: "0487345643",
+     likes: ["Intriguing Cases", "Violin"],
+   },
+   {
+     firstName: "Kristian",
+     lastName: "Vos",
+     number: "unknown",
+     likes: ["JavaScript", "Gaming", "Foxes"],
+   },
+ ];
+
+ function lookUpProfile(name, prop) {
+   // Only change code below this line  && contacts[i].hasOwnProperty(prop)
+   for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name && contacts[i].hasOwnProperty(prop)) {
+         return console.log(contacts[i][prop]);
+      } 
+      if (!(contacts[i].firstName === name)) {
+         return console.log("No such contact")
+      } 
+      if (!contacts[i].hasOwnProperty(prop)) {
+         return console.log("No such property")
+      } 
+   }
+   // return console.log("No such contact")
+   // Only change code above this line
+ }
+
+//  function lookUpProfile(name, prop) {
+//    for (let x = 0; x < contacts.length; x++) {
+//      if (contacts[x].firstName === name) {
+//        if (contacts[x].hasOwnProperty(prop)) {
+//          return console.log(contacts[x][prop]);
+//        } else {
+//          return console.log("No such property")
+//        }
+//      }
+//    }
+//    return console.log("No such contact")
+//  }
+
+ lookUpProfile("Kristian", "lastName") // Vos
+ lookUpProfile("Sherlock", "likes") // ["Intriguing Cases", "Violin"]
+lookUpProfile("Bob", "number") // No such contact
+lookUpProfile("Akira", "address") // No such property
+lookUpProfile("Bob", "potato") // No such contact

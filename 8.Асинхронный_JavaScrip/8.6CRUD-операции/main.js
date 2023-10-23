e8ae3a52974ea244ee2b4b4dcf42e3a182b4f925
PlatonKynin <kininplaton@gmail.com>
2023-10-23 23:16:37 +0300

    8.6 CRUD-операции

--- 8.Асинхронный_JavaScrip/8.6CRUD-операции/main.js
+++ 8.Асинхронный_JavaScrip/8.6CRUD-операции/main.js
@@ -1,3 +1,104 @@
+   // CRUD - create, read, update, delete
+   // HTTP - POST, GET, PUT | PATCH, DELETE
+
+
+  /*  I. */
+
+fetch('https://jsonplaceholder.typicode.com/pots/1')
+   .then(response => {
+      if (response.ok) {
+         return response.json()
+      }
+
+      throw new Error('Failed to fetch');
+   })
+   .then(console.log)
+   .catch(console.error)
+
+
+
+   /* II. */
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+   method: 'DELETE'
+})
+   .then(response => {
+      if (response.ok) {
+         return response.json()
+      }
+
+      throw new Error('Failed to fetch');
+   })
+   .then(console.log)
+   .catch(console.error)
+
+   // Promise {<pending>}
+   // {}
+
+
+
+   /* III. */
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+   method: 'PUT',
+   body: JSON.stringify({id: 1, title: 'My title'}),
+})
+.then(response => {
+   if (response.ok) {
+      return response.json()
+   }
+
+   throw new Error('Failed to fetch');
+})
+.then(console.log)
+.catch(console.error)
+
+   // Promise {<pending>}
+   // {id: 1}
+
+
+
+   /* IV. */
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+   method: 'PATCH',
+   body: JSON.stringify({id: 1, title: 'My title'}),
+})
+.then(response => {
+   if (response.ok) {
+      return response.json()
+   }
+
+   throw new Error('Failed to fetch');
+})
+.then(console.log)
+.catch(console.error)
+
+   // Promise {<pending>}
+   // {userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto'}
+
+
+/* V. */
+fetch('https://jsonplaceholder.typicode.com/posts', {
+   method: 'POST',
+   body: JSON.stringify({id: 201, title: 'My title', body: 'text', userId: 1}),
+})
+   .then(response => {
+      if (response.ok) {
+         return response.json()
+      }
+
+      throw new Error('Failed to fetch');
+   })
+   .then(console.log)
+   .catch(console.error)
+
+
+   // Promise {<pending>}
+   // {id: 101}
+
+   
+   /* VI. */
 fetch('https://jsonplaceholder.typicode.com/posts', {
    method: 'POST',
    body: JSON.stringify({id: 201, title: 'My title', body: 'text', userId: 1}),
@@ -16,5 +117,6 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
    .catch(console.error)
 
 
-   // CRUD - create, read, update, delete
-   // HTTP - POST, GET, PUT | PATCH, DELETE
+   // Promise {<pending>}
+   // {id: 101, title: 'My title', body: 'text', userId: 1}
+

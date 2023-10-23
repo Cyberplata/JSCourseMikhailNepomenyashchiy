a80281281051166702d4054952f47def33775991
PlatonKynin <kininplaton@gmail.com>
2023-10-23 23:28:27 +0300

    8.5 Асинхронное получение данных v2

--- 8.Асинхронный_JavaScrip/8.5Асинхронное_получение_данных/main.js
+++ 8.Асинхронный_JavaScrip/8.5Асинхронное_получение_данных/main.js
@@ -1,40 +1,41 @@
-/* 
-   I.
 
-   fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => {
-         console.log(response)
-      })
+   /* I. */
 
-   // Promise {<pending>}...
-   // Response {type: 'basic', url: 'https://jsonplaceholder.typicode.com/posts/1', redirected: false, status: 200, ok: true, …}
-*/
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+   .then(response => {
+      console.log(response)
+   })
 
-/* 
-   II.
+// Promise {<pending>}...
+// Response {type: 'basic', url: 'https://jsonplaceholder.typicode.com/posts/1', redirected: false, status: 200, ok: true, …}
 
-   fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => response.json())
-      .then(console.log)
 
-   //Promise {<pending>}...
-   //{userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto'}
-*/
 
-/* 
-   III.
+   /* II. */
 
-   fetch('https://jsonplaceholder.typicode.com/pots/1')
-      .then(response => response.json())
-      .then(console.log)
-      .catch(console.error)
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+   .then(response => response.json())
+   .then(console.log)
 
-   //Promise {<pending>}...
-   //GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
-   //{}
-*/
+// Promise {<pending>}...
+// {userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto'}
+
+
+
+   /* III. */
+
+fetch('https://jsonplaceholder.typicode.com/pots/1')
+   .then(response => response.json())
+   .then(console.log)
+   .catch(console.error)
+
+// Promise {<pending>}...
+// GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
+// {}
+
 
    /* IV */
+
 fetch('https://jsonplaceholder.typicode.com/pots/1')
    .then(response => {
       if (response.ok) {
@@ -46,6 +47,6 @@ fetch('https://jsonplaceholder.typicode.com/pots/1')
    .then(console.log)
    .catch(console.error)
 
-   //Promise {<pending>}...
-   //GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
-   //Error: Failed to fetch
+// Promise {<pending>}...
+// GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
+// Error: Failed to fetch

b90f61340ecc67a47a571b538deb73c1126f004c
PlatonKynin <kininplaton@gmail.com>
2023-10-23 22:38:13 +0300

    8.5 Асинхронное получение данных - версия с вариантами

--- 8.Асинхронный_JavaScrip/8.5Асинхронное_получение_данных/main.js
+++ 8.Асинхронный_JavaScrip/8.5Асинхронное_получение_данных/main.js
@@ -1,4 +1,41 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+/* 
+   I.
+
+   fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => {
+         console.log(response)
+      })
+
+   // Promise {<pending>}...
+   // Response {type: 'basic', url: 'https://jsonplaceholder.typicode.com/posts/1', redirected: false, status: 200, ok: true, …}
+*/
+
+/* 
+   II.
+
+   fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(console.log)
+
+   //Promise {<pending>}...
+   //{userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto'}
+*/
+
+/* 
+   III.
+
+   fetch('https://jsonplaceholder.typicode.com/pots/1')
+      .then(response => response.json())
+      .then(console.log)
+      .catch(console.error)
+
+   //Promise {<pending>}...
+   //GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
+   //{}
+*/
+
+   /* IV */
+fetch('https://jsonplaceholder.typicode.com/pots/1')
    .then(response => {
       if (response.ok) {
          return response.json()
@@ -8,3 +45,7 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
    })
    .then(console.log)
    .catch(console.error)
+
+   //Promise {<pending>}...
+   //GET https://jsonplaceholder.typicode.com/pots/1 404 (Not Found)
+   //Error: Failed to fetch

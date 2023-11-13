4d507e7cf2b5e108f398c452772a378cb856ca5d
PlatonKynin <kininplaton@gmail.com>
2023-11-13 23:07:38 +0300

    9.3 Получение данных с сервера

--- 9.Проект_Список_дел/9.2Декомпозиция_на_подзадачи/app.js
+++ 9.Проект_Список_дел/9.2Декомпозиция_на_подзадачи/app.js
@@ -1,16 +1,33 @@
 
+// Globals
+let todos = [];
+let users = [];
+
+// Attach Events
+document.addEventListener('DOMContentLoaded', initApp);
+
+// Event Logic
+function initApp() {
+   Promise.all([getAllTodos(), getAllUsers()]).then(values => {
+      [todos, users] = values;
+
+      
+      // Отправить в разметку
+   })
+}
+
 
 // Asunc logic
 async function getAllTodos() {
    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-   const data = response.json();
+   const data = await response.json();
    
    return data;
 }
 
 async function getAllUsers() {
    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-   const data = response.json();
+   const data = await response.json();
    
    return data;
 }

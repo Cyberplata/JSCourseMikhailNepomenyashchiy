f057589b2f0c7a1db0a214abc044db43897dd07e
PlatonKynin <kininplaton@gmail.com>
2023-12-04 23:07:11 +0300

    9.8 Fix error ...

--- 9.Проект_Список_дел/9.8Удаление_задачи/app.js
+++ 9.Проект_Список_дел/9.8Удаление_задачи/app.js
@@ -30,6 +30,7 @@ function printTodo({id, userId, title, completed}) {
    const close = document.createElement('span');
    close.innerHTML = '&times;';
    close.className = 'close';
+   close.addEventListener('click', handleClose);
 
    li.prepend(status);
    li.append(close);
@@ -48,12 +49,22 @@ function createUserOption(user) {
    userSelect.append(option);
 }
 
+function removerTodo(todoId) {
+   todos = todos.filter(todo => todo.id !== todoId);
+
+   const todo = todoList.querySelector(`[data-id="${todoId}"]`);
+   todo.querySelector('input').removeEventListener('change', handleTodoChange);
+   todo.querySelector('.close').removeEventListener('click', handleClose);
+
+   todo.remove();
+}
+
 // Event Logic
 function initApp() {
    Promise.all([getAllTodos(), getAllUsers()]).then(values => {
       [todos, users] = values;
       console.log(todos, users); // Array(200), Array(10)
-      
+
       // Отправить в разметку
       todos.forEach((todo) => printTodo(todo));
       users.forEach((user) => createUserOption(user))
@@ -80,18 +91,23 @@ function handleTodoChange() {
    toggleTodoComplete(todoId, completed);
 }
 
+function handleClose() {
+   const todoId = this.parentElement.dataset.id;
+   deleteTodo(todoId);
+}
+
 // Async logic
 async function getAllTodos() {
    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
    const data = await response.json();
-   
+
    return data;
 }
 
 async function getAllUsers() {
    const response = await fetch('https://jsonplaceholder.typicode.com/users');
    const data = await response.json();
-   
+
    return data;
 }
 
@@ -127,5 +143,24 @@ async function toggleTodoComplete(todoId, completed) {
 
    if (!response.ok) {
       // Error
+   }
+}
+
+async function deleteTodo(todoId) {
+   const response = await fetch(
+       `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+       {
+          method: 'DELETE',
+          headers: {
+             'Content-Type': 'application/json',
+          },
+       }
+   );
+   // const data = await response.json();
+   // console.log(data);
+
+   if (response.ok) {
+      // remove Todo from DOM
+      removerTodo(todoId);
    }
 }

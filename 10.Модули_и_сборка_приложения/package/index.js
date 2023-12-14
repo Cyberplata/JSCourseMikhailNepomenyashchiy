6fc2be7a04d093554573149b72b259e19e940865
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:42:08 +0300

    Comment first.js export and index.js import Могли бы использовать несколько, если бы потребовалось

--- 10.Модули_и_сборка_приложения/package/index.js
+++ 10.Модули_и_сборка_приложения/package/index.js
@@ -1,4 +1,4 @@
-import {sum} from './first';
+import {sum, addOne} from './first';    // Могли бы использовать несколько, если бы потребовалось
 import {firstName} from './second';
 
 console.log('Hello from entry point', firstName);

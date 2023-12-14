baade1d23d5e89c83c6caecc10c7e8a7f8939232
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:25:45 +0300

    import sum, но js ничего не знает про import - ошибка

--- 10.Модули_и_сборка_приложения/package/first.js
+++ 10.Модули_и_сборка_приложения/package/first.js
@@ -1,14 +1,16 @@
-const x = 1;
+const x = 1;        // Никогда х = 1 не будет доступен
 
-function addOne(num) {
+function addOne(num) {      //Будет доступен, если сделаем export
     return x + num;
 }
 
 
-export function sum(a, b) {         // I вариант
+/* export */ 
+function sum(a, b) {         // I вариант
     return a + b;
 }
 
 export {        // II вариант. Удобно, когда явно видем, что хотим экспортировать
-    sum
+    sum,
+    addOne
 }

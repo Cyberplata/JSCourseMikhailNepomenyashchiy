0d24b27f168fd0cc674f7b2cfc476dd3e5879d02
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:45:02 +0300

    Переименновываем через as index.js

--- 10.Модули_и_сборка_приложения/package/index.js
+++ 10.Модули_и_сборка_приложения/package/index.js
@@ -1,5 +1,6 @@
-import {sum, addOne} from './first';    // Могли бы использовать несколько, если бы потребовалось
+import {sum, addOne as add1} from './first';    // Могли бы использовать несколько, если бы потребовалось. Переименновываем через as
 import {firstName} from './second';
 
 console.log('Hello from entry point', firstName);
 console.log(sum(2, 4));
+console.log(add1(5));

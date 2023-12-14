68a09db049116222cc27df358669c62d747fcd71
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:53:56 +0300

    Как альтернатива записи. Запись через * и объект first

--- 10.Модули_и_сборка_приложения/package/index.js
+++ 10.Модули_и_сборка_приложения/package/index.js
@@ -1,6 +1,9 @@
-import {sum, addOne as add1} from './first';    // Могли бы использовать несколько, если бы потребовалось. Переименновываем через as
+/* import {sum, addOne as add1} from './first'; */    // Могли бы использовать несколько, если бы потребовалось. Переименновываем через as
+import * as first from './first';   // Как альтернатива записи выше. * это всё. first это объект
 import {firstName} from './second';
 
 console.log('Hello from entry point', firstName);
-console.log(sum(2, 4));
-console.log(add1(5));
+// console.log(sum(2, 4));
+// console.log(add1(5));
+console.log(first.sum(2, 4));   // Как альтернатива записи выше
+console.log(first.add1(5));

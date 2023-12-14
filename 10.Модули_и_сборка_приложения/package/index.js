18255ec7fb1826c6ea008d2f7364600b8f20a999
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:36:29 +0300

    export const firstName from second.js and import in index.js

--- 10.Модули_и_сборка_приложения/package/index.js
+++ 10.Модули_и_сборка_приложения/package/index.js
@@ -1,4 +1,5 @@
 import {sum} from './first';
+import {firstName} from './second';
 
-console.log('Hello from entry point');
+console.log('Hello from entry point', firstName);
 console.log(sum(2, 4));

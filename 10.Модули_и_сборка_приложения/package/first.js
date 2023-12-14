43f71250824a9be477cd3f09a105c4757b541142
PlatonKynin <kininplaton@gmail.com>
2023-12-14 20:14:59 +0300

    comment options export to first.js

--- 10.Модули_и_сборка_приложения/package/first.js
+++ 10.Модули_и_сборка_приложения/package/first.js
@@ -1,7 +1,14 @@
-export function sum(a, b) {
+const x = 1;
+
+function addOne(num) {
+    return x + num;
+}
+
+
+export function sum(a, b) {         // I вариант
     return a + b;
 }
 
-export {
+export {        // II вариант. Удобно, когда явно видем, что хотим экспортировать
     sum
 }

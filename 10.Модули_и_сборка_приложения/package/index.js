6d02bdf929852a572b008b8f2d821cad8f75a5bb
PlatonKynin <kininplaton@gmail.com>
2023-12-15 14:32:49 +0300

    changed the import in index.html

--- 10.Модули_и_сборка_приложения/package/index.js
+++ 10.Модули_и_сборка_приложения/package/index.js
@@ -1,9 +1,9 @@
-/* import {sum, addOne as add1} from './first'; */    // Могли бы использовать несколько, если бы потребовалось. Переименновываем через as
-import * as first from './first';   // Как альтернатива записи выше. * это всё. first это объект
+ import {sum, addOne as add1} from './first';    // Могли бы использовать несколько, если бы потребовалось. Переименновываем через as
+// import * as first from './first';   // Как альтернатива записи выше. * это всё. first это объект
 import {firstName} from './second';
 
 console.log('Hello from entry point', firstName);
-// console.log(sum(2, 4));
-// console.log(add1(5));
-console.log(first.sum(2, 4));   // Как альтернатива записи выше
-console.log(first.addOne(5));
+console.log(sum(2, 4));
+console.log(add1(5));
+// console.log(first.sum(2, 4));   // Как альтернатива записи выше
+// console.log(first.addOne(5));

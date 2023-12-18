73b1ab8983e60ce099194c22afe20429abff88f3
PlatonKynin <kininplaton@gmail.com>
2023-12-18 22:55:45 +0300

    10.7 дабавить валидацию введенного IP

--- 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
+++ 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
@@ -1,3 +1,6 @@
+import {validateIp} from './helpers';
+
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
@@ -6,9 +9,11 @@ ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
     // проверка данных
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_6Wxw1bBSgYv8Bw8a0CP1E3cj1oGsh&ipAddress=${ipInput.value}`)
-        .then(response => response.json())
-        .then(console.log);
+    if (validateIp(ipInput.value)) {
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_6Wxw1bBSgYv8Bw8a0CP1E3cj1oGsh&ipAddress=${ipInput.value}`)
+            .then(response => response.json())
+            .then(console.log);
+    }
 }
 
 function handleKey(e) {

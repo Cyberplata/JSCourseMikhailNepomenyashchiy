dda13a3cbb78244290a9ad003bd686c7b03c2cea
PlatonKynin <kininplaton@gmail.com>
2023-12-19 21:19:14 +0300

    11.10 add mapArea, map and L.tileLayer to index.js. Delete .attribution to index.html

--- 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
+++ 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
@@ -1,4 +1,5 @@
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet';
 import {validateIp} from './helpers';
 
 
@@ -12,6 +13,16 @@ const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
+
+const mapArea = document.querySelector('.map');
+const map = L.map(mapArea, {
+    center: [51.505, -0.09],
+    zoom: 13,
+});
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/Cyberplata/JSCourseMikhailNepomenyashchiy/tree/main/11.%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B3%D0%B5%D0%BE%D0%BB%D0%BE%D0%BA%D0%B0%D1%86%D0%B8%D0%B8_%D0%BF%D0%BE_IP/ip-address-tracker-master">Platon Kynin</a>.'
+}).addTo(map);
 
 function getData() {
     // проверка данных

fc9f0455ec8a7f4962f62f0768c6294dc45521e6
PlatonKynin <kininplaton@gmail.com>
2023-12-19 21:42:31 +0300

    Working with popups => circle/polygon/popups as layers

--- 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
+++ 11.Проект-определение_геолокации_по_IP/ip-address-tracker-master/src/index.js
@@ -23,6 +23,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/Cyberplata/JSCourseMikhailNepomenyashchiy/tree/main/11.%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B3%D0%B5%D0%BE%D0%BB%D0%BE%D0%BA%D0%B0%D1%86%D0%B8%D0%B8_%D0%BF%D0%BE_IP/ip-address-tracker-master">Platon Kynin</a>.'
 }).addTo(map);
+L.marker([51.505, -0.09]).addTo(map).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map).bindPopup("I am a circle.");
+L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map).bindPopup("I am a polygon.");
+
+L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
 
 function getData() {
     // проверка данных

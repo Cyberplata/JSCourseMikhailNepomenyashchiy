621e346c15fe6c6d1650ae2176bfb956b2123e47
PlatonKynin <kininplaton@gmail.com>
2024-01-04 19:07:36 +0300

    12.5 Наследование классов es6

---
+++ 12.ООП_в_JavaScript/12.5.Наследование_классов_es6/app.js
@@ -0,0 +1,29 @@
+class Player {  // Новая версия записи
+    constructor(login, score = 100) {
+        this.login = login;
+        this.score = score;
+    }
+    
+    increaseScore(num = 10) {
+        this.score += num;
+    }
+    
+    decreaseScore(num = 10) {
+        this.score -= num;
+    }
+}
+
+class PaidPlayer extends Player {
+    constructor(login, score = 100, accBalance = 1000) {
+        super(login, score);
+        this.accBalance = accBalance;
+    }
+    
+    increaseBalance(amount = 0) {
+        this.accBalance += amount;
+    }
+}
+
+const player3 = new Player('tor');
+const player4 = new Player('loki', 200);
+const pp1 = new PaidPlayer('stark', 200, 100000); 

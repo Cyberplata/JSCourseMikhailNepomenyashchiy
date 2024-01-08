3225740e3f7486874e10c4ac7d711e7df965867e
PlatonKynin <kininplaton@gmail.com>
2024-01-08 22:57:01 +0300

    12.8 Классы для создания UI компонентов v2 - В DOM лишних эл-тов нет. Повторяющегося кода в DOM нет

--- 12.ООП_в_JavaScript/12.8.Классы_для_создания_UI_компонентов/modal.js
+++ 12.ООП_в_JavaScript/12.8.Классы_для_создания_UI_компонентов/modal.js
@@ -5,11 +5,11 @@ class Modal {
         this.init();
     }
 
-    init() {    // Создаём метод init, чтобы он выполнил набор операций
+    init() {  // Создаём метод init, чтобы он выполнил набор операций
         this.createMarkup();
-        this.modal = document.getElementById('myModal');
-        this.closeBtn = this.modal.querySelector('.close');
-        this.attachEvents();
+        this.modal = document.getElementById('myModal');  // В отдельную переменную получаем информацию о корневом эл-те
+        this.closeBtn = this.modal.querySelector('.close');  // В отдельную переменную получаем кнопку закрытия
+        this.attachEvents();  // Обработчики событий привязываем
     }
 
     createMarkup() {
@@ -24,12 +24,29 @@ class Modal {
     }
 
     attachEvents() {
+        this.closeFn = this.closeFn.bind(this);  // Modal, а до этого ссылался на span
+        this.handleClick = this.handleClick.bind(this);
+
         this.closeBtn.addEventListener('click', this.closeFn);
+        window.addEventListener('click', this.handleClick);
+    }
+
+    handleClick(e) {
+        if (e.target === this.modal) {
+            this.closeFn();
+        }
     }
 
     closeFn() {
+        console.log(this);
         // detach events
+        this.detachEvents();
         this.modal.remove();
         this.modal = null;
+    }
+
+    detachEvents() {
+        this.closeBtn.removeEventListener('click', this.closeFn);
+        window.removeEventListener('click', this.handleClick);
     }
 }
